f7cfb38e01b3098903b09e4c2315e5a78f2fb1ee
Marcos Avila <marcosa31@gmail.com>
2022-03-19 00:34:51 -0500

    Use of Fact keyword on test methods

---
+++ OddService/OddService/ExampleServiceImplementation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OddService
+{
+    public class ExampleServiceImplementation
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Third { get; set; }
+
+        public void IncreaseCounters(int number)
+        {
+            if (number > 3 && number <= 5)
+            {
+                First++;
+            }
+            else if (number > 5 && number <= 7)
+            {
+                Second++;
+            }
+            else if (number > 7)
+            {
+                Third++;
+            }
+        }
+    }
+}
---
+++ OddService/OddServiceTests/ExampleServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using OddService;
+
+namespace OddServiceTests
+{
+    public class ExampleServiceTests
+    {
+        [Fact]
+        public void IncreaseCounters_BetweenFiveAndSeven_SecondIncreased()
+        {
+            var service = new ExampleServiceImplementation();
+            service.IncreaseCounters(6);
+
+            Assert.Equal(0, service.First);
+            Assert.Equal(1, service.Second);
+            Assert.Equal(0, service.Third);
+        }
+
+        [Fact]
+        public void IncreaseCounters_BetweenThreeAndFive_FirstIncreased()
+        {
+            var service = new ExampleServiceImplementation();
+            service.IncreaseCounters(4);
+
+            Assert.Equal(1, service.First);
+            Assert.Equal(0, service.Second);
+            Assert.Equal(0, service.Third);
+        }
+
+        [Fact]
+        public void IncreaseCounters_LargerThanSeven_ThirdIncreased()
+        {
+            var service = new ExampleServiceImplementation();
+            service.IncreaseCounters(8);
+
+            Assert.Equal(0, service.First);
+            Assert.Equal(0, service.Second);
+            Assert.Equal(1, service.Third);
+        }
+
+        [Fact]
+        public void IncreaseCounters_SmallerThanThreen_NoCountersIncreased()
+        {
+            var service = new ExampleServiceImplementation();
+            service.IncreaseCounters(1);
+
+            Assert.Equal(0, service.First);
+            Assert.Equal(0, service.Second);
+            Assert.Equal(0, service.Third);
+        }
+    }
+}
