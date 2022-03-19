5ddf67c472f6975c8fe497477dbccbfa996db400
avilamisc <marcos.isc@live.com.mx>
2022-03-18 21:59:15 -0500

    First tests

---
+++ OddService/OddService/OddServiceImplementation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddService
+{
+    public class OddServiceImplementation
+    {
+        public bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+    }
+}
---
+++ OddService/OddServiceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OddServiceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OddServiceTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("140d1ae6-1b52-4205-976e-f5abe5f3f94c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
