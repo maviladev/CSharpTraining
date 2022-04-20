f0f8623f436a048051e9a70ab09fafdf3f9ad310
Marcos Avila <marcosa31@gmail.com>
2022-04-19 21:11:50 -0500

    DbContext
    Added DbContext

---
+++ EFCorePeliculas/EFCorePeliculas/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePeliculas
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- EFCorePeliculas/EFCorePeliculas/Startup.cs
+++ EFCorePeliculas/EFCorePeliculas/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +28,9 @@ namespace EFCorePeliculas
         {
 
             services.AddControllers();
+            var cnnStr = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(opciones =>
+                opciones.UseSqlServer(cnnStr));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
