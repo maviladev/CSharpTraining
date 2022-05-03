0e331094fb03913e4594c466d2b886fe395af9b6
Marcos Avila <marcosa31@gmail.com>
2022-05-02 23:22:19 -0500

    Testing strings
    Use of  Is.EqualTo, Does.Contain, Does.StartWith, Does.EndWith, Assert.IsNull.
    Use of SetUp attribute on method signature of same name to initialize objects and variables.

---
+++ UnitTesting/UnitTestingLibrary/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace UnitTestingLibrary
+{
+    public class Client
+    {
+        public string FullName { get; set; }
+
+        public Client()
+        {
+        }
+
+        public string CreateFullName(string name, string lastName)
+        {
+            FullName = $"{name} {lastName}";
+            return FullName;
+        }
+
+    }
+}
---
+++ UnitTesting/UnitTestingNUnitTest/ClientNUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTestingLibrary
+{
+    [TestFixture]
+    public class ClientNUnitTest
+    {
+        private Client _client { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _client = new Client();
+        }
+
+        [Test]
+        public void CreateFullName_InputTwoStrings_ReturnFullName()
+        {
+            //arrange
+
+            //act
+            _client.CreateFullName("Marcos", "Avila");
+
+            //assert
+            Assert.That(_client.FullName, Is.EqualTo("Marcos Avila"));
+            Assert.AreEqual(_client.FullName, "Marcos Avila");
+
+            Assert.That(_client.FullName, Does.Contain("vil"));     //Case sensitive
+            Assert.That(_client.FullName, Does.StartWith("Mar"));   //Case sensitive
+
+            //Assert.That(_client.FullName, Does.Contain("vil").IgnoreCase); //Compare value
+            //Assert.That(_client.FullName, Does.EndWith("ila")); 
+        }
+
+
+        [Test]
+        public void ClientFullName_InputNoValue_ReturnsNull()
+        {
+            Assert.IsNull(_client.FullName);
+        }
+
+    }
+}
