cf77a0ff8cf98e9a06e2f705090c602f84a85604
Marcos Avila <marcosa31@gmail.com>
2022-05-02 21:11:06 -0500

    Init
    Added MSTest
    Added first test

---
+++ UnitTesting/UnitTestingLibrary/Operation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UnitTestingLibrary
+{
+    public class Operation
+    {
+        public int SumNumbers(int firstNumber, int secondNumber) {
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ UnitTesting/UnitTestingMsTest/OperationMsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestingLibrary;
+
+namespace UnitTestingMsTest
+{
+    [TestClass]
+    public class OperationMsTest
+    {
+        
+        public OperationMsTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void SumNumbers_InputTwoNumbers_GetRightValue()
+        {
+            //Arrange
+            //Area to initialize variables, components, objects
+            Operation op = new();
+
+            int firstNumber = 50;
+            int secondNumber = 69;
+
+            //Act
+            // Execution of the procedure
+            int result = op.SumNumbers(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual(119, result);
+
+        }
+    }
+}
