98ea68dd9f82c77940cf1bb266270e87eb9c7714
Marcos Avila <marcosa31@gmail.com>
2022-05-16 17:46:07 -0500

    Data Annotations

--- projectoef/model/Category.cs
+++ projectoef/model/Category.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace projectoef.models;
 
 public class Category
 {
+    [Key]
     public Guid CategoryId { get; set; }
+    [Required]
+    [MaxLength(150)]
     public string Name { get; set; }
     public string Description { get; set; }
     public virtual ICollection<Task> Tasks { get; set; }
--- projectoef/model/Task.cs
+++ projectoef/model/Task.cs
@@ -1,14 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectoef.models;
 
 public class Task
 {
+    [Key]
     public Guid TaskId { get; set; }
+    [ForeignKey("CategoryId")]
     public Guid CategoryId { get; set; }
+    [Required]
+    [MaxLength(200)]
     public string Title { get; set; }
+    [MaxLength(500)]
     public string Description { get; set; }
+    [Required]
     public Priority PriorityTask { get; set; }
     public DateTime CreationDate { get; set; }
     public virtual Category Category { get; set; }
+    [NotMapped] // Let us omit this field in the db
+    public string Resume { get; set; } 
 }
 
 public enum Priority
