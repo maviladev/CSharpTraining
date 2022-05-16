83a0451ebafddc8e1e7fc031db9fcfbe285851d5
Marcos Avila <marcosa31@gmail.com>
2022-05-16 17:34:08 -0500

    Fix to resolve conflict with class name Task

--- projectoef/TasksContext.cs
+++ projectoef/TasksContext.cs
@@ -1,13 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using projectoef.models;
-
+using projectoef.models;
 namespace projectoef;
  
 
 class TasksContext: DbContext
 {
     public DbSet<Category> Categories { get; set; }
-    public DbSet<Task> Tasks { get; set; }
+    public DbSet<projectoef.models.Task> Tasks { get; set; }
 
     public TasksContext(DbContextOptions<TasksContext> options) : base(options) { }
 
