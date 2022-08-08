0adbbe677bb317a85b9d84c0ee71e98097cf36e2
Marcos Avila <marcosa31@gmail.com>
2022-08-08 07:37:42 -0500

    BudgetManagement

---
+++ ASPMVC/BudgetManagement/BudgetManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
