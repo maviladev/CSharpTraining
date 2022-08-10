0b83a66563721494c18444c7d4302340d11120a6
Marcos Avila <marcosa31@gmail.com>
2022-08-09 21:13:22 -0500

    AccountsTypesController

---
+++ ASPMVC/BudgetManagement/BudgetManagement/Controllers/AccountsTypesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BudgetManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BudgetManagement.Controllers
+{
+    public class AccountsTypesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AccountType accType)
+        {
+            return View();
+        }
+    }
+}
+
---
+++ ASPMVC/BudgetManagement/BudgetManagement/Models/AccountType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BudgetManagement.Models
+{
+	public class AccountType
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public int Order { get; set; }
+
+        public AccountType()
+		{
+		}
+	}
+}
+
